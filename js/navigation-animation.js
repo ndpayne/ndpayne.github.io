985392d1787d1f06dec3326d61e4ba5bad7107e2
Nicholas Payne <ndpayne@comcast.net>
2017-01-23 01:02:04 -0800

    Footer state distinction

--- js/navigation-animation.js
+++ js/navigation-animation.js
@@ -1,4 +1,6 @@
 $('#about').click(function(){
+	$('#welcome').css('text-shadow', 'none');
+	$('#about').css('text-shadow', '.03em .04em #F00');
 	$('#welcome-container').fadeOut(300,function(){
 		$('#about-container').fadeIn(300, function(){
 			$('#about-text').fadeIn(300);
@@ -7,6 +9,8 @@ $('#about').click(function(){
 });
 
 $('#welcome').click(function(){
+	$('#about').css('text-shadow', 'none');
+	$('#welcome').css('text-shadow', '.03em .04em #F00');	
 	$('#about-container').fadeOut(300,function(){
 		$('#welcome-container').fadeIn(300);
 		$('#about-text').css('display', 'none');
